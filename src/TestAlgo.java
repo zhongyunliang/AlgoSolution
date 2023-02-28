d393de046d427763aec24d103dd230e0b636a4ff
zhongyunliang <zhongyunliang404@gmail.com>
2023-02-28 18:50:19 -0500

    some ECE665 Algorithm solutions record

--- src/TestAlgo.java
+++ src/TestAlgo.java
@@ -9,6 +9,7 @@ public class TestAlgo {
         Solution1 solution1 = new Solution1();
         solution1.findEachNodeBalanceFactor(root);
         System.out.println(solution1.res);
+
         int[] arr1 = new int[]{3,23,1,345,2,15,4,56,53,2345,6,2,13,2};
         System.out.println(Arrays.toString(arr1));
         Solution3 solution3 = new Solution3();
@@ -16,9 +17,11 @@ public class TestAlgo {
         System.out.println(Arrays.toString(arr1));
         int[] arr2 = new int[]{3,23,1,345,2,15,4,56,53,2345,6,2,13,2};
         System.out.println(Arrays.toString(arr2));
+
         Solution4 solution4 = new Solution4();
         int k = solution4.heapSelect(arr2, 9);
         System.out.println(k);
+
         int[] arr = new int[]{3,23,1,345,2,15,4,56,53,2345,6,2,13,2};
         Solution2 solution2 = new Solution2();
         long start = System.currentTimeMillis();
